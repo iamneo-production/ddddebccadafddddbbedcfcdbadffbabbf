0e2db29854a42e3c68633888ab4a412547cdca26
Iamneo <techadmin@iamneo.ai>
2023-08-06 12:32:15 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import Stopwatch from './components/Stopwatch/Stopwatch';
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Stopwatch from "./components/Stopwatch";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Link to="/path/to/page">Click me</Link>
       <Stopwatch />
     </div>
   );
